eac2bd767a65a37659d5aa2b8c48a8bc0a092bd6
AJ <terryant64@gmail.com>
2025-07-31 01:35:42 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import ModelViewer from './components/ModelViewer.jsx';
+import NavBar from './components/NavBar.jsx';
 
 function App() {
 
   return (
     <div>
+      <NavBar />
       <ModelViewer />
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,25 @@
 // -------------- //
 // Navigation Bar //
 // -------------- //
+
+function NavBar ({})
+{
+    return (
+        <div name="navbar-name">
+            <p>
+                Anthony Terry
+            </p>
+
+            <ul name="navbar-skills">
+                <l>About</l>
+                <l>Projects</l>
+                <l>Experience</l>
+                <l>Blogs</l>
+            </ul>
+        </div>
+
+
+    );
+}
+
+export default NavBar
